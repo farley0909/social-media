5876391cf25a764e1afd5c512fbc804b751f9f1a
farley0909 <jose.farley@academico.ifpb.edu.br>
2022-12-23 20:22:54 -0300

    Setting home screen design

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { routes } from './routes'
+import * as path from 'path';
 
 const app =  express()
-
+app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.json())
 app.set('view engine', 'ejs');
 app.set('views', './public');
